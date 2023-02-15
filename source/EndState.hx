f4ba8fbbed68830a74e4d0b721a32414c81e309f
Crazyinfy32 <86730481+Crazyinfy32@users.noreply.github.com>
2023-02-15 13:06:39 -0500

    Update EndState.hx

--- source/EndState.hx
+++ source/EndState.hx
@@ -26,7 +26,7 @@ class EndState extends FlxState
 
 	override public function create():Void
 	{
-		FlxG.sound.play(AssetPaths.fail__mp3);
+		FlxG.sound.play(AssetPaths.fail__wav);
 		super.create();
 		FlxG.mouse.visible = true;
 		var windowHeight = FlxG.stage.stageHeight;
