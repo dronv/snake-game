e79c52995303c4073386d5fff7c83b2cf1a3c603
Crazyinfy32 <86730481+Crazyinfy32@users.noreply.github.com>
2023-02-15 00:51:13 -0500

    Add files via upload

---
+++ source/Snakehead.hx
@@ -0,0 +1,25 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+class Snakehead extends FlxSprite
+{
+    public function new(x:Float=0,y:Float=0)
+    {
+        super(x,y);
+        loadGraphic(AssetPaths.snakehead__png,40,40,false);
+        setSize(40,40);
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    }
+
+    public function updatestate(){
+        loadGraphic(AssetPaths.soardragon__png,50,50,false);
+        setSize(50,50);
+        FlxG.sound.play(AssetPaths.levelup__wav);
+    }
+}
